8110708e7d785bc3843fc164863af5eac15c857b
Gabriel Cifuentes <1795@holbertonschool.com>
2020-09-01 21:23:17 +0000

    File added: 8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(str):
+    """prints a string in uppercase followed by a new line"""
+
+    for tchar in str:
+        value = ord(tchar)
+        if value in range(97, 123):
+            value = value - 32
+        print("{:c}".format(value), end="")
+
+    print("")
