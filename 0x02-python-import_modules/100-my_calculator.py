c30f7d26fcd1b4e36c96a6d65f19d8a36d2f790c
Gabriel Cifuentes <1795@holbertonschool.com>
2020-09-03 23:37:49 +0000

    File added: 100-my_calculator.py

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+
+    argv = sys.argv[1:]
+    if len(argv) != 3:
+        sys.stderr.write('Usage: ./100-my_calculator.py <a> <operator> <b>\n')
+        sys.exit(1)
+
+    a = int(argv[0])
+    b = int(argv[2])
+    oper = argv[1]
+
+    if oper not in ['+', '-', '*', '/']:
+        sys.stderr.write('Unknown operator. '
+                         'Available operators: +, -, * and /\n')
+        sys.exit(1)
+    elif argv[1] == '+':
+        print("{} {} {} = {}"
+              .format(a, oper, b, add(a, b)))
+    elif argv[1] == '-':
+        print("{} {} {} = {}"
+              .format(a, oper, b, sub(a, b)))
+    elif argv[1] == '*':
+        print("{} {} {} = {}"
+              .format(a, oper, b, mul(a, b)))
+    elif argv[1] == '/':
+        print("{} {} {} = {}"
+              .format(a, oper, b, div(a, b)))
