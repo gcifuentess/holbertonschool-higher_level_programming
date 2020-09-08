5d31e0e9b90ab422b30f41a37acfee6cd691aca3
Gabriel Cifuentes <1795@holbertonschool.com>
2020-09-08 21:29:00 +0000

    File fixed: 0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    """ prints a list vertically
-
-    args:
-        my_list: a list of ints
-
-    return:
-          void.
-    """
     for i in my_list:
         print("{d}".format(i))
