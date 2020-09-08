a90accb4d23ed15810bf387e22ab142cd1290bd6
Gabriel Cifuentes <1795@holbertonschool.com>
2020-09-08 21:17:59 +0000

    File fixed: 0-print_list_integer.py - docstrig missing

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
+    """ prints a list vertically
+
+    args:
+        my_list: a list of ints
+
+    return:
+          void.
+    """
     for i in my_list:
         print("{}".format(i))
