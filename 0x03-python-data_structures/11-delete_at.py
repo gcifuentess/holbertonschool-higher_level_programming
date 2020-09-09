959f2331c956acd399119e213a45e2e9fab78599
Gabriel Cifuentes <1795@holbertonschool.com>
2020-09-09 21:14:28 +0000

    Files added: 11-delete_at.py; 11-main.py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if not my_list:
-        return None
     new_list = []
     for i in range(0, len(my_list)):
-        if idx != i:
+        if i != idx and i != (len(my_list) + idx):
             new_list.append(my_list[i])
     return new_list
