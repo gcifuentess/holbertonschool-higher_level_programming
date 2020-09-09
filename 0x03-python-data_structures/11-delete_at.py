477ca1fae00021902c85cd086bc16e655de867db
Gabriel Cifuentes <1795@holbertonschool.com>
2020-09-09 21:33:52 +0000

    File fixed: 11-delete_at.py - now works on the original list

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    new_list = []
-    for i in range(0, len(my_list)):
-        if i != idx and i != (len(my_list) + idx):
-            new_list.append(my_list[i])
-    return new_list
+    len_l = len(my_list)
+    if idx < len_l and abs(idx) <= len_l:
+            del my_list[idx]
+    return my_list
