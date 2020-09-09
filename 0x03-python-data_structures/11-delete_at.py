bdeed0330c3a1f8ccecbdf447fc6e271d83721d7
Gabriel Cifuentes <1795@holbertonschool.com>
2020-09-09 21:37:32 +0000

    File fixed: 11-delete_at.py - now do not operatate negative index

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    len_l = len(my_list)
-    if idx < len_l and abs(idx) <= len_l:
+    if idx < len(my_list) and idx >= 0:
             del my_list[idx]
     return my_list
