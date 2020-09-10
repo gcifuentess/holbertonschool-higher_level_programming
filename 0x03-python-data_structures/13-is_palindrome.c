bae0cf2931cb1a10e55d4434e7e6f882a0112b0b
Gabriel Cifuentes <1795@holbertonschool.com>
2020-09-10 04:37:04 +0000

    File added: 13-is_palindrome.c

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * is_palindrome - verifyes is a linked list is  a palindrome
+ * @head: head of the list
+ *
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *single_j, *double_j, *prev, *current, *next;
+	int palindrome = 1;
+
+	if (!(*head) || !((*head)->next))
+		return (palindrome);
+
+	for (single_j = *head, double_j = *head; double_j; )
+	{
+		single_j = single_j->next;
+		if (double_j->next)
+			double_j = (double_j->next)->next;
+	}
+	for (current = single_j, prev = NULL; current; )
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+	for (single_j = prev, current = *head; current->next; )
+	{
+		if (single_j->n == current->n)
+			palindrome = 1;
+		else
+			palindrome = 0;
+		single_j = single_j->next;
+		current = current->next;
+	}
+	for (current = prev, prev = NULL; current != single_j; )
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+	return (palindrome);
+}
