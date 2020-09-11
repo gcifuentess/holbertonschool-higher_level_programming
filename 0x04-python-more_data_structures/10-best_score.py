20044cf7874e99deb6441ca9ddab11c57f28f96a
Gabriel Cifuentes <1795@holbertonschool.com>
2020-09-11 04:20:22 +0000

    File fixed: 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -6,7 +6,7 @@ def best_score(a_dictionary):
         max_score = 0
         keys_a = a_dictionary.keys()
         for key in keys_a:
-            if a_dictionary[key] > max_score:
+            if a_dictionary[key] >= max_score:
                 max_score = a_dictionary[key]
                 max_key = key
     return max_key
