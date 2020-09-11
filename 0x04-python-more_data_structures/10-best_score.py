e1376fb676064050312a35b9e8d79b9a094f0037
Gabriel Cifuentes <1795@holbertonschool.com>
2020-09-11 04:16:22 +0000

    Files added: 10-best_score.py; 10-main.py

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    else:
+        max_score = 0
+        keys_a = a_dictionary.keys()
+        for key in keys_a:
+            if a_dictionary[key] > max_score:
+                max_score = a_dictionary[key]
+                max_key = key
+    return max_key
