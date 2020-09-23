36ab24ef33b9cd77b739982ad34cf1a05fc79ecc
Gabriel Cifuentes <1795@holbertonschool.com>
2020-09-23 04:30:30 +0000

    Files added: 3-main.py 3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Working with Squares"""
+
+
+class Square:
+    """Defines a square"""
+
+    def __init__(self, size):
+        """Creator
+        Args:
+            size: side of the square
+
+        Raises:
+            TypeError: If size is not an integer
+            ValueError: If size is less than 0
+        """
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    def area(self):
+        """Area of the new square
+        Returns:
+            The area
+        """
+        return self.__size ** 2
