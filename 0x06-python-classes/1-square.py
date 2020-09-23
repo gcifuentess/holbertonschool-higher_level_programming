12d6210604895ae108fcce110c37b6de43ff7345
Gabriel Cifuentes <1795@holbertonschool.com>
2020-09-23 04:08:14 +0000

    Files added: 1-square.py 1-main.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Working with Squares"""
+
+
+class Square:
+    """Defines a square"""
+
+    def __init__(self, size):
+        """Creator
+        Args:
+            size: side of the square
+        """
+        self.__size = size
