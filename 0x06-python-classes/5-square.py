d51008e82356f353765122cbef2adb6801a83a7e
Gabriel Cifuentes <1795@holbertonschool.com>
2020-09-23 04:52:52 +0000

    Files added: 5-main.py 5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Working with Squares"""
+
+
+class Square:
+    """Defines a square"""
+
+    def __init__(self, size=0):
+        """Creator
+        Args:
+            size: side of the square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Property for the length of a side of this square.
+
+        Raises:
+            TypeError: If size is not an integer
+            ValueError: If size is less than 0
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, val):
+        if not isinstance(val, int):
+            raise TypeError('size must be an integer')
+        if val < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = val
+
+    def area(self):
+        """Area of the new square
+        Returns:
+            The area
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """Prints square."""
+        for i in range(self.size):
+            for j in range(self.size):
+                print("#", end="\n" if j is self.size - 1 and i != j else "")
+        print()
