30cf79ea255b5264f5d5c298d9ed74ab7ed6f83c
Gabriel Cifuentes <1795@holbertonschool.com>
2020-09-25 19:56:39 +0000

    Files added: 4-main.py 4-print_square.py tests/4-print_square.txt

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,22 @@
+"""print_square module"""
+
+
+def print_square(size):
+    """Prints a square with # chars
+    Args:
+        size: The int size of the square's side
+    Raises:
+        TypeError: when size is not an int
+        ValueError: when size is < 0.
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    print((("#" * size + "\n") * size), end="")
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/4-print_square.txt")
