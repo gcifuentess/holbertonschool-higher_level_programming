13a4c7da3e617f58e0f935bfda254db9f063d87c
Gabriel Cifuentes <1795@holbertonschool.com>
2020-09-29 18:33:46 +0000

    File fixed: 101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
-'''N Queens'''
+'''N Queens Problem'''
 
 
 def validation(chessboard, row, column):
-    '''validates current position.
+    '''validates current position to see if its available
+       vs the queens already set on the columns to the left.
     Args:
         chessboard: actual state of the game.
         row: row to validate.
         column: column to validate.
     '''
     for col in range(column):
+        # checks if there is no queen in the row or diagonal
         if (chessboard[col] == row or
-                abs(chessboard[col] - row) == abs(col - column)):
+                # checks the slope:
+                abs(col - column) == abs(chessboard[col] - row)):
             return False
     return True
 
@@ -23,6 +26,8 @@ def backtracking(chessboard, column):
         column: the colum to backtrack,
     '''
     q = len(chessboard)
+    # when all the queens are set and the validation is True,
+    # prints the solution
     if column == q:
         solution = []
         for col in range(q):
@@ -31,6 +36,7 @@ def backtracking(chessboard, column):
         return
 
     for row in range(q):
+        # if validation is True, a new queen is set and start to test a new one
         if validation(chessboard, row, column):
             chessboard[column] = row
             backtracking(chessboard, column + 1)
@@ -50,7 +56,10 @@ if __name__ == "__main__":
     if q < 4:
         print("N must be at least 4")
         sys.exit(1)
+
+    # Creation of the chessboard
     chessboard = []
     for col in range(q):
         chessboard.append(col)
+    # starts the seeking of the solution in the first column
     backtracking(chessboard, 0)
