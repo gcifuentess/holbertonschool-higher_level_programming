c9aeacaced7d05045c20313cab9f6a1c720a55f8
Gabriel Cifuentes <1795@holbertonschool.com>
2020-09-30 13:57:57 +0000

    Files fixed: 2-matrix_divided.py tests/2-matrix_divided.txt

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -21,18 +21,18 @@ def matrix_divided(matrix, div):
     if type(div) not in (int, float):
         raise TypeError("div must be a number")
     if type(matrix) is not list or len(matrix) == 0:
-        raise TypeError("matrix must be a matrix (list of lists) of "
-                        + "integers/floats")
+        raise TypeError("matrix must be a matrix (list of lists) of " +
+                        "integers/floats")
     for row in matrix:
         if type(matrix) is not list or len(row) == 0:
-            raise TypeError("matrix must be a matrix (list of lists) of "
-                            + "integers/floats")
+            raise TypeError("matrix must be a matrix (list of lists) of " +
+                            "integers/floats")
         if len(row) != len(matrix[0]):
             raise TypeError("Each row of the matrix must have the same size")
         for x in row:
             if type(x) not in (int, float):
-                raise TypeError("matrix must be a matrix (list of lists) of "
-                                + "integers/floats")
+                raise TypeError("matrix must be a matrix (list of lists) of " +
+                                "integers/floats")
     return [[round(x / div, 2) for x in row] for row in matrix]
 
 if __name__ == "__main__":
