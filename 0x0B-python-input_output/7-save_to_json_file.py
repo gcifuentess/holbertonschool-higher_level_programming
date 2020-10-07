304575c89e3ad2f7704b0d1e4d82d3bfc85e2a69
Gabriel Cifuentes <1795@holbertonschool.com>
2020-10-07 23:21:49 +0000

    File fixed: 7-save_to_json_file.py

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -5,6 +5,6 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """writes an Object to a text file, using a JSON representation"""
-    encode = json.dumps(my_obj)
+
     with open(filename, mode='w', encoding='utf-8') as a_file:
-        a_file.write(encode)
+        json.dump(my_obj, a_file)
