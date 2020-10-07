316c939e3fc74648cef58ba198ba6ea73b371076
Gabriel Cifuentes <1795@holbertonschool.com>
2020-10-07 19:08:43 +0000

    Files added: 0-read_file.py 0-main.py README.md

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""IO module"""
+
+def read_file(filename=""):
+    """Reads a text file (UTF8) and prints it to stdout."""
+    with open(filename, encoding='utf-8') as a_file:
+        for line in a_file:
+            print(line, end='')
