e9421cb4fd2fab9bb4a2d388243564e73a37ae1a
Gabriel Cifuentes <1795@holbertonschool.com>
2020-10-07 03:17:45 +0000

    Files added: 6-base_geometry.py 6-main.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Geometry Module"""
+
+
+class BaseGeometry():
+    """Base Geometry Constructor"""
+
+    def area(self):
+        """Raises Exception"""
+        raise Exception("area() is not implemented")
