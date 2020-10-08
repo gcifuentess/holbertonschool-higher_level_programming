e9059d12732b646e8846f7adf371908f77515200
Gabriel Cifuentes <1795@holbertonschool.com>
2020-10-08 03:03:35 +0000

    Files added: 12-student.py 12-main.py

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Student to JSON module"""
+
+
+class Student():
+    """Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Student constructor"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''Returns dictionary description of json object filtered'''
+        flag = False
+        if (type(attrs) is list):
+            flag = True
+            for attr in attrs:
+                if type(attr) is not str:
+                    flag = False
+        if flag is True:
+            my_dict = {}
+            for key, value in self.__dict__.items():
+                if key in attrs:
+                    my_dict[key] = value
+        else:
+            my_dict = self.__dict__.copy()
+
+        return my_dict
