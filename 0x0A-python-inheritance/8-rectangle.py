bb0596eba4849632f6440ebc877a5e4c361ce124
Gabriel Cifuentes <1795@holbertonschool.com>
2020-10-09 00:20:45 +0000

    Files added: 8-rectangle.py 8-main.py

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Rectangle Module"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle subclass"""
+
+    def __init__(self, width, height):
+        """Rectangle Constructor"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__width = height
