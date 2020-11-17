4ff66182cf77a8a0f3ea9191117f1ea3a3468a94
Gabriel Cifuentes <1795@holbertonschool.com>
2020-11-17 02:00:06 +0000

    Files for tasks 0 to 5

---
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -0,0 +1,2 @@
+-- list all the databases in the system
+SHOW DATABASES;
---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+-- Creates a table called first_table in the current database
+CREATE TABLE first_table (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(256));
