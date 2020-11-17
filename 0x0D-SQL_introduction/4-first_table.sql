715a9a145b3476b028d2c766a2f0e451f7a266f8
Gabriel Cifuentes <1795@holbertonschool.com>
2020-11-17 02:22:25 +0000

    Added 4-first_table.sql 7-insert_value.sql

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,2 @@
 -- Creates a table called first_table in the current database
-CREATE TABLE first_table (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-name VARCHAR(256));
+CREATE TABLE first_table (id INT, name VARCHAR(256));
