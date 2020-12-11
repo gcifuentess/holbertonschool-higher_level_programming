418feaeaef5f5184d8c2afdaeefcb14236b93933
Gabriel Cifuentes <1795@holbertonschool.com>
2020-12-11 22:07:51 +0000

    add README.md 0-select_states.sql  0-select_states.py

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''Query with MySQLdb'''
+import MySQLdb
+import sys
+
+
+argv = sys.argv
+db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2],
+                     db=argv[3], charset="utf8")
+cur = db.cursor()
+cur.execute("SELECT * FROM states ORDER BY id ASC")
+query_rows = cur.fetchall()
+for row in query_rows:
+    print(row)
+cur.close()
+db.close()
