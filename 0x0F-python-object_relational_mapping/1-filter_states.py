6e548b18e4c3d97c2ae017b680664aa8c0c66f2a
Gabriel Cifuentes <1795@holbertonschool.com>
2020-12-12 23:09:09 +0000

    fix 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,7 +11,8 @@ if __name__ == "__main__":
                          passwd=argv[2], db=argv[3], charset="utf8")
     cur = db.cursor()
     cur.execute("\
-    SELECT * FROM states WHERE states.name REGEXP '^N' ORDER BY states.id ASC;")
+    SELECT * FROM states WHERE states.name REGEXP '^N' \
+    ORDER BY states.id ASC;")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
