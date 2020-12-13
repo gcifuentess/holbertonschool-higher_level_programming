ec784da0eef946fb921bf9ab339a2c6c9c91786b
Gabriel Cifuentes <1795@holbertonschool.com>
2020-12-13 19:19:41 +0000

    fix 8-model_state_fetch_first.py

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""
+'''
 Query with SQLAlchemy, first State object from the database hbtn_0e_6_usa
-"""
+'''
 from sys import argv
 from model_state import Base, State
 from sqlalchemy import (create_engine)
@@ -17,8 +17,8 @@ if __name__ == "__main__":
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    my_query = session.query(State).order_by(State.id)
-    if my_query:  # my_query.count() != 0:
+    my_query = session.query(State).order_by(State.id).first()
+    if my_query.count() != 0:
         print("{}: {}".format(my_query[0].id, my_query[0].name))
     else:
         print("Nothing")
