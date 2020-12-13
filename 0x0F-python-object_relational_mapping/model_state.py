d7167e259eb6375a9091814c93753dd21e43c776
Gabriel Cifuentes <1795@holbertonschool.com>
2020-12-13 01:26:12 +0000

    add model_state.py 6-model_state.py 6-model_state.sql

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''first ORM model - state'''
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    '''class definition of a State'''
+
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
