e91cc952a1698c7759323679bbd0700f774d9688
Gabriel Cifuentes <1795@holbertonschool.com>
2020-12-13 16:59:58 +0000

    add 8-model_state_fetch_first.py

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''
+Query with SQLAlchemy, first State object from the database hbtn_0e_6_usa
+'''
+
+
+if __name__ == "__main__":
+    from sys import argv
+    from model_state import Base, State
+    from sqlalchemy import (create_engine)
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           encoding='utf-8', pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    my_query = session.query(State).order_by(State.id)
+    if my_query.count() != 0:
+        print("{}: {}".format(my_query[0].id, my_query[0].name))
+    else:
+        print("Nothing")
