69deabcaecf2a95f212c207a94f0c844a86dc16d
Gabriel Cifuentes <1795@holbertonschool.com>
2020-12-13 19:14:26 +0000

    fix 8-model_state_fetch_first.py

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-'''
+"""
 Query with SQLAlchemy, first State object from the database hbtn_0e_6_usa
-'''
+"""
 from sys import argv
 from model_state import Base, State
 from sqlalchemy import (create_engine)
