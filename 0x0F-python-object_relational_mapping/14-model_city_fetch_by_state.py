2a3864481914aeeee9c1ab641dbab37a2499713f
Gabriel Cifuentes <1795@holbertonschool.com>
2020-12-13 22:19:31 +0000

    add 14-model_city_fetch_by_state.py 14-model_city_fetch_by_state.sql model_city.py

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Query with SQLAlchemy,prints all City objects
+from the database hbtn_0e_14_usa
+"""
+
+if __name__ == "__main__":
+    from sys import argv
+    from model_state import Base, State
+    from model_city import City
+    from sqlalchemy import (create_engine)
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           encoding='utf-8', pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    my_query = session.query(City, State).filter(City.state_id == State.id).\
+        order_by(City.id)
+    for city, state in my_query:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    session.close()
