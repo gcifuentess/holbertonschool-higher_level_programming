47b0545ecd456cb8d4cb679affca71afb245485e
Gabriel Cifuentes <1795@holbertonschool.com>
2020-12-13 17:52:02 +0000

    fix 8-model_state_fetch_first.py

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -12,13 +12,13 @@ if __name__ == "__main__":
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(argv[1], argv[2], argv[3]),
-                           pool_pre_ping=True)
+                           encoding='utf-8', pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
     my_query = session.query(State).order_by(State.id)
-    if my_query.count() != 0:
+    if my_query: # my_query.count() != 0:
         print("{}: {}".format(my_query[0].id, my_query[0].name))
     else:
         print("Nothing")
