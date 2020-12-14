f52dd76b721f07f3031fa84c679fd07eebae3fef
Gabriel Cifuentes <1795@holbertonschool.com>
2020-12-14 00:20:52 +0000

    fix relationship_state.py

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -14,4 +14,4 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete")
+    cities = relationship("City", backref="state", cascade="all, delete-orphan")
