d3bad68b642725446a3e693b3ee518b410dc4a2c
Gabriel Cifuentes <1795@holbertonschool.com>
2020-12-14 00:18:46 +0000

    fix model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 '''first ORM model - state'''
-from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
