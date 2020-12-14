4a3cab79a27b84722a8063e03f36a9b40d085603
Gabriel Cifuentes <1795@holbertonschool.com>
2020-12-14 00:13:41 +0000

    add 100-relationship_states_cities.py relationship_state.py relationship_city.py 100-relationship_states_cities.sql

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Query with SQLAlchemy, creates the State “California” with the
+City “San Francisco” from the database hbtn_0e_100_usa
+"""
+
+if __name__ == "__main__":
+    from sys import argv
+    from model_state import Base, State
+    from model_city import City
+    from sqlalchemy import (create_engine)
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           encoding='utf-8', pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    new_state = State(name="California")
+    session.add(new_state)
+    session.commit()
+    new_city = City(name="San Francisco", state_id=new_state.id)
+    session.add(new_city)
+    session.commit()
+
+    session.close()
---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''first ORM model - state'''
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    '''class definition of a State'''
+
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
