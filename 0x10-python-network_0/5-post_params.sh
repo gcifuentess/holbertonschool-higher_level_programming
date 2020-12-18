c03dd1f2fd1665111668c0fcca2b9a4ea893a18c
Gabriel Cifuentes <1795@holbertonschool.com>
2020-12-18 22:16:35 +0000

    fix 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # POST method with parameters
-curl -s --data "email=hr@holbertonschool.com&subject=I will always be here for PLD"
+curl -s --data "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
