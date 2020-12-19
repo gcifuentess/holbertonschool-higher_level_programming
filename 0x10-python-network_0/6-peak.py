39af7b32bd253ab08b3295e50cd171e70a28a416
Gabriel Cifuentes <1795@holbertonschool.com>
2020-12-19 03:19:30 +0000

    add 6-peak.py 6-peak.txt

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""find-peak algorithm module"""
+
+
+def find_peak(list_of_integers):
+    """finds a peak in a list of unsorted integers"""
+
+    if list_of_integers:
+        list_i = list_of_integers
+        if len(list_i) > 2:
+            pivot = int(len(list_i) / 2)
+            peak = list_i[pivot]
+            if peak > list_i[pivot - 1] and peak > list_i[pivot + 1]:
+                return peak
+            elif peak > list_i[pivot + 1]:
+                return find_peak(list_i[:pivot])
+            else:
+                return find_peak(list_i[pivot + 1:])
+        else:
+            return max(list_i)
+    else:
+        return None
