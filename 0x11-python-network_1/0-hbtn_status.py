e00144ede5e279e0d41cd5405f6eacadaa933cff
Gabriel Cifuentes <1795@holbertonschool.com>
2021-01-11 16:52:29 +0000

    add README.md 0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''urllib module'''
+import urllib.request
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        request = response.read()
+
+        request_utf8 = request.decode('utf8')
+        print("Body response:\n \
+        \t- type: {}\n \
+        \t- content: {}\n \
+        \t- utf8 content: {}".format(type(request), request, request_utf8))
