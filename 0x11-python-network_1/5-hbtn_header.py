0fb3d021e316165e350f9534e828cfca8da6a8a6
Gabriel Cifuentes <1795@holbertonschool.com>
2021-01-11 21:05:22 +0000

    add 5-hbtn_header.py

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''request module'''
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    r = requests.get(argv[1])
+    request = r.headers['X-Request-Id']
+    print(request)
