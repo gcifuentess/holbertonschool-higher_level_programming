736c391feb09e07128021a3c5c95e3a13b281995
Gabriel Cifuentes <1795@holbertonschool.com>
2021-01-12 00:58:55 +0000

    add 100-github_commits.py

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''request module'''
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = 'https://api.github.com/repos/' +\
+          argv[1] + '/' + argv[2] + '/commits'
+    r = requests.get(url)
+    r = r.json()
+    commits = 10
+    for commit in r:
+        if commits > 0:
+            commits -= 1
+            sha = commit.get('sha')
+            author = commit.get('commit').get('author').get('name')
+            print("{}: {}".format(sha, author))
+        else:
+            break
