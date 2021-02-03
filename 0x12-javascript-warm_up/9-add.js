5fa120e04ef37edda25128d048cd64e51df22df0
Gabriel Cifuentes <1795@holbertonschool.com>
2021-02-03 02:45:49 +0000

    add 9-add.js

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// prints the addition of 2 integers
+const num1 = parseInt(process.argv[2]);
+const num2 = parseInt(process.argv[3]);
+console.log(add(num1, num2));
+
+function add (a, b) {
+  return a + b;
+}
