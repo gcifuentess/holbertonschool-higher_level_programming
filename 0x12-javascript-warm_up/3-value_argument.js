6c6d06363f6b565b8411e9e8ddcb901315e68785
Gabriel Cifuentes <1795@holbertonschool.com>
2021-02-03 02:04:02 +0000

    fix 3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -4,9 +4,5 @@ const Argv2 = process.argv[2];
 if (!Argv2) {
   console.log('No argument');
 } else {
-  process.argv.forEach((val, index) => {
-    if (index > 1) {
-      console.log(val);
-    }
-  });
+  console.log(Argv2);
 }
