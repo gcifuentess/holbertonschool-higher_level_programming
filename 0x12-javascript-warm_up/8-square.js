01cfc4c844a2c9f764ac6f3ba33638fb37c60aa3
Gabriel Cifuentes <1795@holbertonschool.com>
2021-02-03 02:36:15 +0000

    add 8-square.js

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// prints a square
+const cast = parseInt(process.argv[2]);
+let i = 0;
+let j = 0;
+let square = '';
+
+if (!cast) {
+  console.log('Missing size');
+} else {
+  for (i = 0; i < cast; i++) {
+    for (j = 0; j < cast; j++) {
+      square = square + '#';
+    }
+    square = square + '\n';
+  }
+  console.log(square);
+}
