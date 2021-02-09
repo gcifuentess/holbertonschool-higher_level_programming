f9a536e2e492a538ab144d22c1a325aced97acf7
Gabriel Cifuentes <1795@holbertonschool.com>
2021-02-09 19:22:12 +0000

    fix 2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,6 +3,6 @@
 const request = require('request');
 const url = process.argv[2];
 request(url, function (error, response, body) {
-  console.error('error:', error); // Print the error if one occurred
+  if (error) console.error('error:', error); // Print the error if one occurred
   console.log('code:', response.statusCode); // Print the response status code if a response was received
 });
