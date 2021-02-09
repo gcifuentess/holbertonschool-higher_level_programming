b3584bb8ab7efcb52238859d466090d8a8b2ccac
Gabriel Cifuentes <1795@holbertonschool.com>
2021-02-09 01:16:58 +0000

    add 3-rectangle.js 3-main.js

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// Add print method to Rectangle class
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+    this.print = function () {
+      let strR = 'X';
+      strR = strR.repeat(w) + '\n';
+      strR = strR.repeat(h);
+      console.log(strR.slice(0, strR.length - 1));
+    };
+  }
+}
+
+module.exports = Rectangle;
