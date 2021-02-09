4b4cac8485986e4703bbefaea80f8705871cd9e9
Gabriel Cifuentes <1795@holbertonschool.com>
2021-02-09 19:16:33 +0000

    add 2-statuscode.js

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// display the status code of a GET request.
+const request = require('request');
+const url = process.argv[2];
+request(url, function (error, response, body) {
+  console.error('error:', error); // Print the error if one occurred
+  console.log('Code:', response.statusCode); // Print the response status code if a response was received
+});
