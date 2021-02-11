3702e941c5c16d5aad5630844f461c2e787baf1d
Gabriel Cifuentes <1795@holbertonschool.com>
2021-02-11 02:59:31 +0000

    add 4-script.js 4-main.html

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,7 @@
+// toggles the class of the HTML tag HEADER when the user clicks on the
+// tag DIV#toggle_header:
+// The HEADER tag must always have one class: red or green, never both in the
+//   same time, never empty.
+// If the current class is red, when the user click on DIV#toggle_header, the
+//   class must be updated to green ; and the reverse.
+$('DIV#toggle_header').click(() => $('HEADER').toggleClass('red green'));
